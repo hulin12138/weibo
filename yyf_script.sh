4b02233760e97cddb4952161c8cf0b536c9131da
yyf <143@>
2017-03-21 21:25:40 +0800

    yyf 03-21

---
+++ yyf_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+BAYESDIR=/home/yyf/Documents/codes/pycharm/ML/
+GITDIR=/home/yyf/Documents/ml/introduction2ml/weibo/
+
+
+export PYTHONPATH=$PYTHONPATH:$BAYESDIR:$BAYESDIR/weibo
+
+
+
+
+
+
