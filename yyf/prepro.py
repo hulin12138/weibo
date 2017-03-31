6b6aad53816c5e2f5cae4f2a4ff2a094fca3528c
yyf <143@>
2017-03-31 15:19:46 +0800

    2017年 03月 31日 星期五 15:19:46 CST

--- yyf/prepro.py
+++ yyf/prepro.py
@@ -27,29 +27,34 @@ USEBOOTSTRAP = False
 #     print(len(files))
 #     print(deal_passage(os.path.join(DATADIR, dir, files[1])))
 #     break
-
+# 从文件夹中读取数据到dataset对象中,利用其中的函数进行处理
 dataset = read_data(DATADIR)
 
 if USENVIVEBAYES:
     precisions = []
-    for d in range(1, 11, 2):
-        topk = [1, 2]
-        tpre = []
-        for test,train in dataset.partition():
-            pre = []
-            nb = NaiveBayes(test, train, dataset.labels, d / 10.)
-            nb.train()
-            for k in topk:
-                pre.append(nb.precision(k))
-            # pre.append(nb.train_precision())
-            tpre.append(pre)
-        precisions.append(np.mean(tpre, axis=0))
+    d = 0.6
+    topk = [1, 2]
+    tpre = []
+    # test, train 是讲将所有的data分成训练和测试数据
+    # 他们的格式形如[(label, passage), (label, passage), ...]
+    # 每个passage是词的list, 形如[word, word, ...]
+    for test,train in dataset.partition():
+        pre = []
+        nb = NaiveBayes(test, train, dataset.labels, d / 10.)
+        nb.train()
+        # 测试topk的准确率
+        for k in topk:
+            pre.append(nb.precision(k))
+        # 测试训练的准确率
+        # pre.append(nb.train_precision())
+        tpre.append(pre)
+    precisions.append(np.mean(tpre, axis=0))
 
     print ("Final results:")
     print(precisions)
 
 if GRADIENT_CHECK:
-
+    # 神经网络的梯度检查, 确保求得的梯度是正确的.
     model = Softmax(False, 2, 2)
     model.regular = 0.1
     model.use_regular = True
@@ -74,6 +79,8 @@ if GRADIENT_CHECK:
     model.gradient_check(X, Y)
 
 def bootstrap(X, y, k=3):
+    # 将每个passage进行采用,扩大样本的大小,
+    # 每个词被选中的概率为(k-1)/k
     bx, by = [], []
     for idx, x in enumerate(X):
         x = np.array(x)
@@ -100,6 +107,9 @@ if USESOFTMAX:
         print(len(test))
         print(len(train))
         tx, ty = [], []
+        # 将每个词转换成在词表中的indice
+        # trainx形如[[w1, w2, ..], [w1, w2, ..], ...]
+        # trainy: [lable1, lable2, ...]
         for lbl, psg in test:
             ty.append(label2idx[lbl])
             tx.append([])
@@ -116,6 +126,7 @@ if USESOFTMAX:
             trainx.extend(bx)
             trainy.extend(by)
             print(len(trainx))
+        # 将处理好的数据传递给model
         model.fit(trainx, trainy)
         for run in range(30):
             print("\nrun %d:"%(run))
